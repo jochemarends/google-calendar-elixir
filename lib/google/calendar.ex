7b1ebc78db2bf445f8c335789d695fa1e4c534f3
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2025-10-06 11:27:58 +0200

    first commit

---
+++ lib/google/calendar.ex
@@ -0,0 +1,18 @@
+defmodule Google.Calendar do
+  @moduledoc """
+  Documentation for `Google.Calendar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Google.Calendar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/google/calendar_test.exs
@@ -0,0 +1,8 @@
+defmodule Google.CalendarTest do
+  use ExUnit.Case
+  doctest Google.Calendar
+
+  test "greets the world" do
+    assert Google.Calendar.hello() == :world
+  end
+end
